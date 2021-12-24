8b224a396323078966188888ce1e064d5fa01829
Val Hustle <stars@MacBook-Air.local>
2021-12-24 18:11:24 -0500

    setting up the header to have the punkLogo images and the search bar inputted making sure its styled to look like a real website

--- react-nft-challenge/src/components/Header.js
+++ react-nft-challenge/src/components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import './Header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
 
 const Header = () => {
-    return <h2>I'm the Header 🚀</h2>
-}
+  return (
+    <div className = "header">
+        <div className ='logoContainer'>
+            <img src = {punkLogo} className ='punkLogo' alt='' /> 
+        </div>
+        <div className = "searchBar">
+            <div className = "searchIconContainer">
+              <img src={searchIcon} />
+            </div>
+            <input className='searchInput'/>
+        </div>
+    </div> 
+  );
+};
 
-export default Header
+export default Header;
